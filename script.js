020f04443744ad423ac9f7ae8ea6641ae92065c9
Joaoguilhermeasf <135768526+Joaoguilhermeasf@users.noreply.github.com>
2024-02-28 01:09:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+ScrollReveal().reveal('.txtcontainer', { delay: 550 });
+
+
+ScrollReveal().reveal('.voluntarios', { delay: 550 });
+
+ScrollReveal().reveal('.pg2imgs', { delay: 850 });
+
+
+
+ScrollReveal().reveal('.sidebartxt', { delay: 500 });
+
+ScrollReveal().reveal('.parceiros1', { delay: 550 });
+
+ScrollReveal().reveal('.parceiros2', { delay: 550 });
+
+
+ScrollReveal().reveal('.txtcontainer2', { delay: 500 });
+ScrollReveal().reveal('.pg5foto1', { delay: 600 });
+ScrollReveal().reveal('.pg5foto2', { delay: 700 });
+ScrollReveal().reveal('.pg5foto3', { delay: 800 });
+ScrollReveal().reveal('.pg5foto4', { delay: 900 });
+
+
